35f232ab767ce8b80c8259768cfea596db7742be
Kyle Galloway <123207266+Kyle-Galloway01@users.noreply.github.com>
2023-11-27 17:07:27 -0600

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,34 @@
+public class BinarySearch {
+    // Iterative binary search function
+    static int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Check if target is present at the mid
+            if (arr[mid] == target)
+                return mid;
+
+            // If target greater, ignore left half
+            if (arr[mid] < target)
+                left = mid + 1;
+
+            // If target is smaller, ignore right half
+            else
+                right = mid - 1;
+        }
+        // Target not found
+        return -1;
+    }
+
+    // Example usage
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 4, 10, 40 };
+        int target = 10;
+        int result = binarySearch(arr, target);
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at index " + result);
+    }
+}
